21e8ad6293bf190153b2fda8c26797cecc6f9eb1
Sergei Ryzhko <sergei.ryzhko@proton.me>
2025-08-13 12:15:06 +0300

    ass

--- init.lua
+++ init.lua
@@ -41,9 +41,6 @@ vim.pack.add({
 
 vim.cmd.colorscheme "gruvbox"
 
--- Plugin configurations
-
-
 --- [[
 ------------------ mini.nvim START ------------------
 -- https://github.com/echasnovski/mini.basics
@@ -63,32 +60,6 @@ require('mini.basics').setup({
 		relnum_in_visual_mode = true,
 	},
 })
-
--- https://github.com/echasnovski/mini.base16
--- require('mini.base16').setup({
--- 	palette = {
--- 		base00 = "#181818",
--- 		base01 = "#282828",
--- 		base02 = "#383838",
--- 		base03 = "#585858",
--- 		base04 = "#b8b8b8",
--- 		base05 = "#d8d8d8",
--- 		base06 = "#e8e8e8",
--- 		base07 = "#f8f8f8",
--- 		base08 = "#ab4642",
--- 		base09 = "#dc9656",
--- 		base0A = "#f7ca88",
--- 		base0B = "#a1b56c",
--- 		base0C = "#86c1b9",
--- 		base0D = "#7cafc2",
--- 		base0E = "#ba8baf",
--- 		base0F = "#a16946",
--- 	},
--- 	use_cterm = nil,
--- 	plugins = {
--- 		default = true,
--- 	},
--- })
 
 -- https://github.com/echasnovski/mini.comment
 require('mini.comment').setup()
