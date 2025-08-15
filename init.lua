3562b6d26d5548d353b7cc2cb503e3a586d9975a
Sergei Ryzhko <sergei.ryzhko@proton.me>
2025-08-15 15:24:37 +0300

    who? cares?

--- init.lua
+++ init.lua
@@ -1,57 +1,75 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
--- Кирил
+-- Кирил <- spellcheck check check
 -- Most of defaults are in mini.basics
 vim.opt.swapfile = false
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-vim.opt.langmap="ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+vim.opt.langmap =
+"ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
 vim.opt.confirm = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.spelllang = { "en_us", "ru_ru" }
 vim.opt.scrolloff = 5
+vim.opt.winborder = ""
+vim.opt.foldmethod = "expr"
+vim.opt.foldcolumn = "0"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
+-- vim.opt.background = "dark"
 vim.opt.list = false
+vim.g.loaded_python3_provider = 0
 vim.opt.listchars = {
-	tab = "» ",     -- Display tabs as ▸ followed by a space
-	trail = "·",    -- Show trailing spaces as middle dots
-	eol = " ",      -- Show end-of-line as a carriage return symbol
-	space = "⋅",    -- Show spaces as small dots
-	nbsp = "␣",     -- Show non-breaking spaces as a specific symbol
-	extends = "→",  -- Show when a line extends beyond the screen
+	tab = "» ", -- Display tabs as ▸ followed by a space
+	trail = "·", -- Show trailing spaces as middle dots
+	eol = " ", -- Show end-of-line as a carriage return symbol
+	space = "⋅", -- Show spaces as small dots
+	nbsp = "␣", -- Show non-breaking spaces as a specific symbol
+	extends = "→", -- Show when a line extends beyond the screen
 	precedes = "←", -- Show when a line is preceded from the left
 }
 
-vim.keymap.set("n", "<Leader>so", ":update | source ~/.config/nvim/init.lua<CR>")
+-- Enable lsp's
+vim.lsp.enable({ "lua_ls", "gopls", "systemd_ls", "hyprls", "bashls", "yamlls", "docker-language-server" })
+vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end)
+vim.keymap.set("n", "<Leader>o", ":update | source ~/.config/nvim/init.lua<CR>")
 vim.keymap.set("n", "<Leader>n", ":messages<CR>")
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
-
-vim.keymap.set({ 'n', 'x' }, 'j', [[v:count == 0 ? 'gj' : 'j']], { expr = true })
-vim.keymap.set({ 'n', 'x' }, 'k', [[v:count == 0 ? 'gk' : 'k']], { expr = true })
-
-vim.keymap.set({ 'n', 'x' }, "<leader>y", '"+y"')
-vim.keymap.set({ 'n', 'x' }, "<leader>p", '"+p"')
+vim.keymap.set("n", "<Leader>lf", function() vim.lsp.buf.format() end, {})
 
 -- Plugin installations
 vim.pack.add({
-	{ src = "https://github.com/echasnovski/mini.nvim", version = 'main' },
-	{ src = "https://github.com/folke/flash.nvim", version = 'main' },
-	{ src = "https://github.com/ellisonleao/gruvbox.nvim", version = 'main' },
+	{ src = "https://github.com/echasnovski/mini.nvim",           version = 'main' },
+	{ src = "https://github.com/folke/flash.nvim",                version = 'main' },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim",        version = 'main' },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = "https://github.com/Saghen/blink.cmp",                version = "v1.6.0" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets",    version = "main" },
+	{ src = "https://github.com/christoomey/vim-tmux-navigator",    version = "master" },
+	{ src = "https://github.com/f-person/auto-dark-mode.nvim",    version = "master" },
 })
 
+-- Plugin configurations
+require("gruvbox").setup({
+	transparent_mode = false,
+	overrides = {
+		-- Pmenu = { bg = "#282828" },
+	},
+})
 vim.cmd.colorscheme "gruvbox"
-
 --- [[
------------------- mini.nvim START ------------------
 -- https://github.com/echasnovski/mini.basics
 require('mini.basics').setup({
 	options = {
 		basic = true,
-		extra_ui = true,
-		win_borders = 'solid',
+		extra_ui = false,
+		win_borders = '',
 	},
 	mappings = {
-		basic = false,
+		basic = true,
 		option_toggle_prefix = "<Leader>t",
 		windows = true,
 	},
@@ -70,16 +88,16 @@ require('mini.pairs').setup()
 -- https://github.com/echasnovski/mini.surround
 require('mini.surround').setup({
 	mappings = {
-		add = 'ms', -- Add surrounding in Normal and Visual modes
-		delete = 'md', -- Delete surrounding
-		replace = 'mr', -- Replace surrounding
-		find = 'gsf', -- Find surrounding (to the right)
-		find_left = 'gsF', -- Find surrounding (to the left)
-		highlight = 'gsh', -- Highlight surrounding
+		add = 'ms',           -- Add surrounding in Normal and Visual modes
+		delete = 'md',        -- Delete surrounding
+		replace = 'mr',       -- Replace surrounding
+		find = 'gsf',         -- Find surrounding (to the right)
+		find_left = 'gsF',    -- Find surrounding (to the left)
+		highlight = 'gsh',    -- Highlight surrounding
 		update_n_lines = 'gsn', -- Update `n_lines`
 
-		suffix_last = 'l', -- Suffix to search with "prev" method
-		suffix_next = 'n', -- Suffix to search with "next" method
+		suffix_last = 'l',    -- Suffix to search with "prev" method
+		suffix_next = 'n',    -- Suffix to search with "next" method
 	},
 })
 
@@ -99,6 +117,9 @@ require('mini.statusline').setup()
 -- https://github.com/echasnovski/mini.icons
 require('mini.icons').setup()
 
+-- https://github.com/echasnovski/mini.cursorword
+require('mini.cursorword').setup()
+
 -- https://github.com/echasnovski/mini.indentscope
 require('mini.indentscope').setup({
 	draw = {
@@ -111,10 +132,10 @@ require('mini.indentscope').setup({
 require('mini.hipatterns').setup({
 	highlighters = {
 		-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-		fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-		hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-		todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-		note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+		fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+		hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+		todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+		note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
 
 		-- Highlight hex color strings (`#rrggbb`) using that color
 		hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
@@ -122,7 +143,7 @@ require('mini.hipatterns').setup({
 })
 
 -- https://github.com/echasnovski/mini.starter
-require('mini.starter').setup()
+-- require('mini.starter').setup()
 
 -- https://github.com/echasnovski/mini.pick
 require('mini.pick').setup({
@@ -130,26 +151,37 @@ require('mini.pick').setup({
 		use_cahce = true,
 	},
 })
-
 vim.keymap.set("n", "<Leader>f", function() MiniPick.builtin.files() end)
 vim.keymap.set("n", "<Leader>h", function() MiniPick.builtin.help() end)
 vim.keymap.set("n", "<Leader>b", function() MiniPick.builtin.buffers() end)
 vim.keymap.set("n", "<Leader>r", function() MiniPick.builtin.resume() end)
 vim.keymap.set("n", "<Leader>/", function() MiniPick.builtin.grep_live() end)
-vim.keymap.set("n", "<Leader>d", function() MiniExtra.pickers.diagnostics() end)
 vim.keymap.set("n", "<Leader>?", function() MiniExtra.pickers.history() end)
-vim.keymap.set("n", "<Leader>j", function() MiniExtra.pickers.list( { scope = "quickfix" }) end)
+vim.keymap.set("n", "<Leader>j", function() MiniExtra.pickers.list({ scope = "quickfix" }) end)
 vim.keymap.set("n", '<Leader>"', function() MiniExtra.pickers.registers() end)
-vim.keymap.set("n", '<Leader>ss', function() MiniExtra.pickers.spellsugest() end)
+-- vim.keymap.set("n", '<Leader>ss', function() MiniExtra.pickers.spellsuggest() end)
+vim.keymap.set("n", "<Leader>d", function() MiniExtra.pickers.diagnostic({ scope = "current"}) end)
+vim.keymap.set("n", "<Leader>D", function() MiniExtra.pickers.diagnostic({ scope = "all"}) end)
+vim.keymap.set("n", '<Leader>s', function() MiniExtra.pickers.lsp({ scope = 'document_symbol' }) end)
+vim.keymap.set("n", '<Leader>S', function() MiniExtra.pickers.lsp({ scope = 'workspace_symbol' }) end)
+vim.keymap.set("n", 'gD', function() MiniExtra.pickers.lsp({ scope = 'declaration' }) end)
+vim.keymap.set("n", 'gd', function() MiniExtra.pickers.lsp({ scope = 'definition' }) end)
+vim.keymap.set("n", 'gi', function() MiniExtra.pickers.lsp({ scope = 'implementation' }) end)
+vim.keymap.set("n", 'gr', function() MiniExtra.pickers.lsp({ scope = 'references' }) end)
+vim.keymap.set("n", 'gt', function() MiniExtra.pickers.lsp({ scope = 'type_definition' }) end)
 vim.keymap.set("n", "<Leader>gb", function() MiniExtra.pickers.git_branches() end)
 vim.keymap.set("n", "<Leader>gc", function() MiniExtra.pickers.git_commits() end)
 vim.keymap.set("n", "<Leader>gf", function() MiniExtra.pickers.git_files() end)
 vim.keymap.set("n", "<Leader>gh", function() MiniExtra.pickers.git_hunks() end)
 vim.keymap.set("n", "<Leader>mh", function() MiniExtra.pickers.hl_groups() end)
+vim.keymap.set("n", "<Leader>mp", function() MiniExtra.pickers.hipatterns() end)
 vim.keymap.set("n", "<Leader>mk", function() MiniExtra.pickers.keymaps() end)
-
 -- https://github.com/echasnovski/mini.files
-require('mini.files').setup({})
+require('mini.files').setup({
+	options = {
+		permanent_delete = false,
+	},
+})
 vim.keymap.set("n", "<Leader>e", function() MiniFiles.open() end)
 
 -- https://github.com/echasnovski/mini.git
@@ -159,11 +191,18 @@ require('mini.git').setup()
 require('mini.diff').setup()
 
 -- https://github.com/echasnovski/mini.sessions
-require('mini.sessions').setup()
-vim.keymap.set("n", "<Leader>sw", function()
-	vim.ui.input({ prompt = 'Enter session name:' }, function(input)
-		MiniSessions.write(input)
-	end)
+require('mini.sessions').setup({
+	autoread = false,
+	autowrite = true,
+})
+
+vim.keymap.set("n", "<Leader>mw", function()
+	local session_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+	MiniSessions.write(session_name)
+end)
+vim.keymap.set("n", "<Leader>mr", function()
+	local session_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+	MiniSessions.read(session_name)
 end)
 
 -- https://github.com/echasnovski/mini.diff
@@ -188,5 +227,63 @@ require("flash").setup({
 		},
 	}
 })
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end)
 
-vim.keymap.set({"n", "x", "o"}, "s", function() require("flash").jump() end)
+-- https://github.com/nvim-treesitter/nvim-treesitter
+local ensureInstalled = {
+	"lua",
+	"go",
+	"gomod",
+	"gosum",
+	"gotmpl",
+	"gowork",
+	"css",
+	"html",
+	"css",
+	"javascript", "typescript",
+	"yaml",
+	"toml",
+	"ini",
+	"systemd",
+	"hyprls"
+}
+require("nvim-treesitter").install(ensureInstalled)
+
+-- https://github.com/Saghen/blink.cmp
+require("blink.cmp").setup({
+	cmdline = { enabled = false },
+	completion = {
+		accept = { auto_brackets = { enabled = false }, },
+		list = { selection = { preselect = false, auto_insert = false } },
+	},
+	keymap = {
+		preset = 'default',
+		['<C-x>'] = {'show_signature', 'hide_signature'},
+		['<C-l>'] = {'snippet_forward'},
+		['<C-h>'] = {'snippet_backward'},
+	},
+	signature = {
+		enabled = true,
+	},
+	appearance = {
+		nerd_font_variant = 'mono',
+		use_nvim_cmp_as_default = false,
+	},
+	fuzzy = {
+	},
+})
+
+-- https://github.com/christoomey/vim-tmux-navigator
+vim.keymap.set({"n", "i"}, "<C-l>", "<Cmd>TmuxNavigateRight<CR>")
+vim.keymap.set({"n", "i"}, "<C-h>", "<Cmd>TmuxNavigateLeft<CR>")
+vim.keymap.set({"n", "i"}, "<C-k>", "<Cmd>TmuxNavigateUp<CR>")
+vim.keymap.set({"n", "i"}, "<C-j>", "<Cmd>TmuxNavigateDown<CR>")
+
+require("auto-dark-mode").setup({
+	set_dark_mode = function ()
+		vim.api.nvim_set_option_value("background", "dark", {})
+	end,
+	set_light_mode = function ()
+		vim.api.nvim_set_option_value("background", "light", {})
+	end
+})
---
+++ lsp/lua_ls.lua
@@ -0,0 +1,54 @@
+local mini_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/mini.nvim/lua"
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+					mini_path
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library'
+          -- '${3rd}/busted/library'
+        }
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+})
+
+return {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = {
+    '.luarc.json',
+    '.luarc.jsonc',
+    '.luacheckrc',
+    '.stylua.toml',
+    'stylua.toml',
+    'selene.toml',
+    'selene.yml',
+    '.git',
+  },
+}
+
